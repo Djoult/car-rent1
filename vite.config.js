2861242140de4b71aa49bd6d01ce88d1ab62b233
Djoult <55453354+Djoult@users.noreply.github.com>
2023-09-17 08:30:17 +0300

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  base: "/car-rent1/",
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+    },
+  },
+});
